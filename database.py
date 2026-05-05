ad1c451c20b2dca0434780ca37254f3cc547e5f0
Aparna Nema <136079269+n10aparna@users.noreply.github.com>
2026-05-05 13:38:29 +0530

    initial commit

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-dburl = "postgresql://postgres:harshit99@localhost:5432/mydatabase"
+dburl = "postgresql://postgres:password@localhost:5432/mydatabase"
 engine = create_engine(dburl)
 session = sessionmaker(autoflush=False,autocommit=False,bind=engine)
